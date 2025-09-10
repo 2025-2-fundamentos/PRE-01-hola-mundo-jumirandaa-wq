37f94f16c6d313917c96f27da2b0c91b4ae54e6e
jumirandaa-wq <jumirandaa@unal.edu.co>
2025-09-10 09:42:23 -0500

    Update pregunta_01.py

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,7 +5,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 # pylint: disable=import-outside-toplevel
 
 
-def pregunta_01():
+def pregunta_01(n):
     """
     Retorne el string "Hola mundo cruel!".
 
@@ -14,7 +14,7 @@ def pregunta_01():
 
     """
 
-    return "Hola mundo cruel!"
+    print("Hola mundo cruel!")
 
 
 if __name__ == "__main__":
